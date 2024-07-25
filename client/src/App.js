6af9b07affda8b668c076b9e7c77f9f640e6971c
Heather Dice <heatherdice22@gmail.com>
2024-07-24 22:45:57 -0400

    Added component, css file for component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Home from "./pages/home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Home />
     </div>
   );
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState, useRef, useCallback } from "react";
+import { debounce } from "lodash";
+import { Link } from 'react-scroll';
+
+
+export default function Navbar() {
+    const [screenType, setScreenType] = useState(window.innerWidth <= 600 ? 'mobile' : 'desktop');
+
+    const debouncedHandleResize = useRef(
+        debounce(() => {
+            setScreenType(window.innerWidth <= 600 ? 'mobile' : 'desktop');
+        }, 300)).current;
+
+    const handleResize = useCallback(() => {
+        debouncedHandleResize();
+        }, [debouncedHandleResize]);
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+            debouncedHandleResize.cancel();
+        };
+    }, [handleResize, debouncedHandleResize]);
+
+    const navLinks = [
+        {
+            link: 'about',
+            title: 'About'
+        },
+        {
+            link: 'book-a-horse',
+            title: 'Book a Horse'
+        },
+        {
+            link: 'event-calendar',
+            title: 'Event Calendar'
+        }
+    ];
+
+    return (
+        <>
+            <nav className="navbar">
+                <p className="site-title">
+                    <span>Elysian Fields</span>
+                    <span>Horse Rentals</span>
+                </p>
+
+                {screenType === 'desktop' ? (
+                    <ul className="desktop-nav">
+                        {navLinks.map((navbar) =>
+                            <li key={navbar.title} className="desktop-navbar-links">
+                                <Link to={navbar.link} smooth={true} duration={500}>
+                                    {navbar.title}
+                                </Link>
+                            </li>
+                        )}
+                    </ul>
+                ) : (
+                    <div className="menu">Menu</div>
+                )}
+            </nav>
+        </>
+    )
+}
+
