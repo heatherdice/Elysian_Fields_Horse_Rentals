9f31b91ba937fc4ac265a373c98c446c812952ea
Heather Dice <heatherdice22@gmail.com>
2024-07-24 21:47:31 -0400

    Installs

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Home from "./pages/home";
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Home />
+    </div>
   );
 }
 
