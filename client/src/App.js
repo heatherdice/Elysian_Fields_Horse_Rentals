b9edfbbe5b92f8f9cb7464ce02ab9db74352a111
Heather Dice <heatherdice22@gmail.com>
2024-07-29 22:05:17 -0400

    Took out Home temporarily, added Hero, React

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
+import React from "react";
 import './App.css';
-import Home from "./pages/home";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home />
+      <Hero />
     </div>
   );
 }
