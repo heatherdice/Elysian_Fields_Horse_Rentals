bcc2281bc763b007ee34e604a1ad996d0b629e80
Heather Dice <heatherdice22@gmail.com>
2024-07-30 23:57:51 -0400

    Began adding next section to home page

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -6,6 +6,15 @@ export default function Home() {
     return (
         <>
             <Hero image={homeHero} title="Elysian Fields" subtitle="Horse Rentals" />
+
+            <section style={{backgroundColor: "var(--hunter-green)", color: "var(--platinum)"}}>
+                <h3 style={{fontSize: "2.5em", fontWeight: "normal", margin: "0"}}>
+                    Horses suitable for IHA, IHSA, mounted archery, and more!
+                </h3>
+                <p>
+                    Elysian Fields Horse Rentals has been renting out horses for competition and pleasure for [x years]. Our horses are trained to be calm, focused, and non-reactive to outside stimuli, ensuring a safe and enjoyable ride! From small local schooling shows to big exciting IHSA competitions, our horses have done it all.
+                </p>
+            </section>
         </>
     )
 }
