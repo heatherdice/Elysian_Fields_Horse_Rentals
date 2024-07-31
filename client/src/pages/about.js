758692859b0ac5450ba605ef01ae8af5b4a0f043
Heather Dice <heatherdice22@gmail.com>
2024-07-31 12:01:25 -0400

    Added about page, about hero image

---
+++ client/src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "../components/Hero";
+import aboutHero from "../assets/aboutHero.jpg"
+
+export default function about() {
+    return (
+        <>
+            <Hero image={aboutHero} title="About Us" subtitle="" />
+
+        </>
+    )
+}
