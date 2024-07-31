da8488e1c009d85adbaee5846a12157e45debd34
Heather Dice <heatherdice22@gmail.com>
2024-07-30 23:50:04 -0400

    Improvements to hero responsiveness, placement

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -2,25 +2,25 @@ import React from "react";
 
 export default function Hero({ image }) {
     return(
-        <div style={{ backgroundImage: `url(${image})` }} className="heroImage">
-            <h1 style={
-                {marginLeft: "60px",
-                marginTop: "0", 
-                paddingTop: "50px", 
-                marginBottom: "0", 
-                fontSize: "3em"}
-            }>
-                Elysian Fields
-            </h1>
-            <h2 style={
-                {marginLeft: "60px", 
-                marginTop: "16px", 
-                fontSize: "2em", 
-                fontWeight: "normal",
-                paddingBottom: "50px"}
-            }>
-                Horse Rentals
-            </h2>
+        <div style={{ backgroundImage: `url(${image})` }} className="hero-image">
+            <div className="hero-text">
+                <h1 style={
+                    {marginTop: "0", 
+                    paddingTop: "50px", 
+                    marginBottom: "0", 
+                    fontSize: "3em"}
+                }>
+                    Elysian Fields
+                </h1>
+                <h2 style={
+                    {marginTop: "16px", 
+                    fontSize: "2em", 
+                    fontWeight: "normal",
+                    paddingBottom: "50px"}
+                }>
+                    Horse Rentals
+                </h2>
+            </div>
         </div>
     )
 }
