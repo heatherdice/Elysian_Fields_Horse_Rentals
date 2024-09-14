0b4e3ee19817f5cf0dd5ea5c004857405dce9f86
Heather Dice <heatherdice22@gmail.com>
2024-09-13 21:17:45 -0400

    Made Hero styled-component

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { styled } from "styled-components";
+
+const HeroContainer = styled.header`
+    width: 100%;
+    height: 70vh;
+    background-position: 50% 25%;
+    background-size: cover;
+    background-repeat: no repeat;
+    @media (max-width: 900px) {
+        height: 40vh;
+    }
+`;
 
 export default function Hero({ image, homePage, title }) {
     return(
-        <header style={{ backgroundImage: `url(${image})` }} className="Hero">
+        <HeroContainer style={{ backgroundImage: `url(${image})` }} className="Hero">
             {!homePage ? (
                 <div className="hero-text">
                     <h1>{title}</h1>
                 </div>
             ) : null}
-        </header>
+        </HeroContainer>
     )
 }
