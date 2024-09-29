0b1388c09ce0d9f04db7e41ca0623a35fde83fa0
Heather Dice <heatherdice22@gmail.com>
2024-09-29 17:47:11 -0400

    Styling for affiliate section

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -1,7 +1,10 @@
 import React from "react";
 import Hero from "../components/Hero";
-import aboutHero from "../assets/aboutHero.jpg"
-import { AboutPage, AboutInfo, AboutHorses, AffiliateLinks } from "../styles/About.styles";
+import aboutHero from "../assets/aboutHero.jpg";
+import ulhaLogo from "../assets/ulhalogo.png";
+import ieaLogo from "../assets/iea.png";
+import ihsaLogo from "../assets/ihsalogo.png";
+import { AboutPage, AboutInfo, AboutHorses, AffiliateLinks, AffiliateLogos } from "../styles/About.styles";
 
 export default function about() {
     return (
@@ -14,11 +17,18 @@ export default function about() {
                 </AboutInfo>
 
                 <AboutHorses>
+                    <h3>Meet Our Horses</h3>
 
                 </AboutHorses>
 
                 <AffiliateLinks>
-                    
+                    <h3>Our Affiliates</h3>
+
+                    <AffiliateLogos src={ulhaLogo} alt="United Liberty Horse Archers" />
+
+                    <AffiliateLogos src={ieaLogo} alt="Interscholastic Equestrian Association" />
+
+                    <AffiliateLogos src={ihsaLogo} alt="Intercollegiate Horse Shows Association" />
                 </AffiliateLinks>
             </AboutPage>
 
--- client/src/styles/About.styles.js
+++ client/src/styles/About.styles.js
@@ -8,6 +8,28 @@ const AboutPage = styled.main`
 const AboutSections = styled.section`
     display: grid;
     height: 70vh;
+    h3 {
+        font-size: 2.5em;
+        font-weight: normal;
+        place-self: center;
+    }
+    p {
+        font-size: 1.5em;
+    }
+    @media (max-width: 900px) {
+        padding: 2em;
+        column-gap: 1em;
+        h3 {
+            font-size: 1.75em;
+        }
+        p {
+            font-size: 1em;
+        }
+    }
+    @media (max-width: 450px) {
+        row-gap: 1em;
+        padding: 1em;
+    }
 `;
 
 const AboutInfo = styled(AboutSections)`
@@ -19,11 +41,29 @@ const AboutInfo = styled(AboutSections)`
 const AboutHorses = styled(AboutSections)`
     color: var(--champagne-pink);
     background-color: var(--english-violet);
+    grid-template-columns: 1fr 1fr;
+    column-gap: 3em;
+    h3 {
+        grid-column: span 2;
+        place-self: center;
+    }
 `;
 
 const AffiliateLinks = styled(AboutSections)`
     color: var(--hunter-green);
     background-color: var(--champagne-pink);
+    grid-template-columns: 1fr 1fr 1fr;
+    column-gap: 3em;
+    h3 {
+        grid-column: span 3;
+
+    }
+`;
+
+const AffiliateLogos = styled.img`
+    grid-column: span 1;
+    height: 33%
+    width: 33%;
 `;
 
 export {
@@ -31,5 +71,6 @@ export {
     AboutSections,
     AboutInfo,
     AboutHorses,
-    AffiliateLinks
+    AffiliateLinks,
+    AffiliateLogos
 }
