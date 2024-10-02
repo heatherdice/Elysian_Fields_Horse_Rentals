f12db35152269422a9fa450614d1b5dd055c773d
Heather Dice <heatherdice22@gmail.com>
2024-10-02 16:54:54 -0400

    Consolidated all horse info to one page

--- client/src/pages/horses.js
+++ client/src/pages/horses.js
@@ -1,24 +1,59 @@
 import React from "react";
 import Hero from "../components/Hero";
 import horsesHero from "../assets/horsesHero.jpg";
-import { Link } from "react-router-dom";
-import { AllHorsesPage, FilterHorsesRow } from "../styles/AllHorses.styles.js";
+import archeryHorsesHero from "../assets/archeryHorse.jpg";
+import jumperHorsesHero from "../assets/jumperHorse.jpg";
+import { Link, useParams } from "react-router-dom";
+import { AllHorsesPage, FilterHorsesRow } from "../styles/horses.styles.js";
 
 
 export default function Horses() {
+    // get category from url
+    const { category } = useParams();
+
+    // conditionally render Hero content
+    const getHeroContent = () => {
+        if (category === "archery") {
+            return { image: archeryHorsesHero, title: "Archery Horses"};
+        } else if (category === "jumper") {
+            return { image: jumperHorsesHero, title: "Jumper Horses"};
+        } else {
+            return { image: horsesHero, title: "All Horses"};
+        }
+    };
+    const heroContent = getHeroContent();
+
+    // conditionally render links
+    const renderLinks = () => {
+        const linksMap = {
+            archery: [
+                {to: "/horses/all", label: "See All Horses"},
+                {to: "/horses/jumper", label: "See Jumper Horses"}
+            ],
+            jumper: [
+                {to: "/horses/archery", label: "See Archery Horses"},
+                {to: "/horses/all", label: "See All Horses"}
+            ],
+            default: [
+                {to: "/horses/archery", label: "See Archery Horses"},
+                {to: "/horses/jumper", label: "See Jumper Horses"}
+            ]
+        }
+
+        const currentLinks = linksMap[category] || linksMap.default;
+
+        return currentLinks.map((link, index) => (
+            <Link key={index} to={link.to}>{link.label}</Link>
+        ))
+    };
+
     return (
         <>
-            <Hero image={horsesHero} homePage={false} title="All Horses" />
+            <Hero image={heroContent.image} homePage={false} title={heroContent.title} />
 
             <AllHorsesPage>
                 <FilterHorsesRow>
-                    <Link to="/archery-horses">
-                        See Archery Horses
-                    </Link>
-
-                    <Link to="/jumper-horses">
-                        See Jumper Horses
-                    </Link>
+                    {renderLinks()}
                 </FilterHorsesRow>
             </AllHorsesPage>
         </>
