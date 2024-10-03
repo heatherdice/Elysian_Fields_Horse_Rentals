44263fd047333abbb1d795c01c29fd9b0c8b6c68
Heather Dice <heatherdice22@gmail.com>
2024-10-03 05:19:00 -0400

    Added HorseCard component and styles files

---
+++ client/src/components/HorseCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { CardContainer } from "../styles/HorseCard.styles";
+
+export default function HorseCard() {
+    return (
+        <CardContainer>
+            This is a sample card
+        </CardContainer>
+    )
+}
---
+++ client/src/styles/HorseCard.styles.js
@@ -0,0 +1,12 @@
+import { styled } from "styled-components";
+
+const CardContainer = styled.article`
+    background: var(--platinum);
+    color: var(--hunter-green);
+    border-radius: 8px;
+    width: 80%;
+`;
+
+export {
+    CardContainer
+}
