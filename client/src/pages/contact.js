88c0bd13e2640172eeb81099ba1f6ce768763b62
Heather Dice <heatherdice22@gmail.com>
2024-10-04 19:36:00 -0400

    Added contact page

---
+++ client/src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero from "../components/Hero";
+import contactHero from "../assets/aboutHero.jpg";
+
+export default function contact() {
+    return (
+        <>
+            <Hero image={contactHero} homepage={false} title="Contact Us" />
+
+            
+        </>
+    )
+}
