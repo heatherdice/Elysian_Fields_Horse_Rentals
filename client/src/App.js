c23bb0474bba3cfcc8189fb57dca83307b4a45ec
Heather Dice <heatherdice22@gmail.com>
2024-10-04 20:04:00 -0400

    Changed contact page to calendar

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Horses from "./pages/horses";
+import Calendar from "./pages/calendar";
 import Footer from "./components/Footer";
 
 function App() {
@@ -18,7 +19,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/horses" component={<Horses />} />
           <Route path="/horses/:category" element={<Horses />} />
-
+          <Route path="/calendar" element={Calendar} />
         </Routes>
 
         <Footer backgroundColor="var(--english-violet)" textColor="var(--platinum)" />
---
+++ client/src/pages/calendar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "../components/Hero";
+
+export default function calendar() {
+    return (
+        <>
+            <Hero image={{ style:{ backgroundColor: "var(--platinum)", color: "var(--hunter-green)" } }} homepage={false} title="Contact Us" />
+
+            
+        </>
+    )
+}
