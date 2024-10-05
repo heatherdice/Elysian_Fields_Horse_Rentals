4e6f0ac68823b92a823181f60324223af4ab9740
Heather Dice <heatherdice22@gmail.com>
2024-10-05 18:17:00 -0400

    Added note - may make separate admin app

--- client/src/pages/admin.js
+++ client/src/pages/admin.js
@@ -3,6 +3,6 @@ import { AdminPage } from "../styles/admin.styles";
 
 export default function admin() {
     <AdminPage>
-    
+        {/* Considering making a separate app for admin page/operations. Need to do more reading on what the best option here would be. */}
     </AdminPage>
 }
